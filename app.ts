939cfb110c4e3dd6349fcd5d92dae8648d65a6f3
mechtatel <alaxche@gmail.com>
2024-04-09 22:45:20 +1000

    updateKT2

--- app.ts
+++ app.ts
@@ -103,97 +103,144 @@ admin.grade = 10
 
 
 // КТ 2
+abstract class Publisher {
+  title: string;
+  author: string;
+  pubYear: number;
+  copies: number;
 
-class User{
-    public static count = 0;
-    public name: string;
-    private readonly login: string;
-    private password: string;
-    private readonly grade: number;
-    constructor( name:string, login:string, password:string, grade:number) {
-      this.name = name;
-      this.login = login;
-      this.password = password;
-      this.grade = grade;
-      User.count++;
+  constructor(title: string, author: string, pubYear: number, copies: number) {
+    this.title = title;
+    this.author = author;
+    this.pubYear = pubYear;
+    this.copies = copies;
+  }
+
+  getTitle(): string {
+    return this.title;
+  }
+
+  setTitle(title: string): void {
+    this.title = title;
+  }
+
+  getAuthor(): string {  
+    return this.author;
+  }
+
+  setAuthor(author: string): void {
+    this.author = author;
+  }
+
+  getPubYear(): number {
+    return this.pubYear;
+  }
+
+  setPubYear(pubYear: number): void {
+    this.pubYear = pubYear;
+  }
+
+  getCopies(): number {
+    return this.copies;
+  }
+
+  setCopies(copies: number): void {
+    this.copies = copies;
+  }
+}
+
+class Book extends Publisher {
+  pages: number;
+
+  constructor(title: string, author: string, pubYear: number, copies: number, pages: number) {
+    super(title, author, pubYear, copies);
+    this.pages = pages;
+  }
+}
+
+class Magazine extends Publisher {
+  issue: number;
+
+  constructor(title: string, author: string, pubYear: number, copies: number, issue: number) {
+    super(title, author, pubYear, copies);
+    this.issue = issue;
+  }
+}
+
+interface Reception {
+  delivery(reader: Reader, publisher: Publisher): void;
+  receive(reader: Reader, publisher: Publisher): void;
+}
+
+class Reader {
+  firstName: string;
+  lastName: string;
+  items: Publisher[];
+  maxItems: number;
+
+  constructor(firstName: string, lastName: string, maxItems: number) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.items = [];
+    this.maxItems = maxItems;
+  }
+
+  getFirstName(): string {
+    return this.firstName;
+  }
+
+  setFirstName(firstName: string): void {
+    this.firstName = firstName;
+  }
+
+  getLastName(): string {
+    return this.lastName;
+  }
+
+  setLastName(lastName: string): void {
+    this.lastName = lastName;
+  }
+
+  getItems(): Publisher[] {
+    return this.items;
+  }
+
+  setItems(items: Publisher[]): void {
+    this.items = items;
+  }
+
+  getMaxItems(): number {
+    return this.maxItems;
+  }
+
+  setMaxItems(maxItems: number): void {
+    this.maxItems = maxItems;
+  }
+}
+
+class Library {
+  publishers: Publisher[];
+
+  constructor(publishers: Publisher[]) {
+    this.publishers = publishers;
+  }
+
+  getPublishers(): Publisher[] {
+    return this.publishers;
+  }
+
+  setPublishers(publishers: Publisher[]): void {
+    this.publishers = publishers;
+  }
+
+  addPublisher(publisher: Publisher): void {
+    this.publishers.push(publisher);
+  }
+
+  removePublisher(publisher: Publisher): void {
+    const index = this.publishers.indexOf(publisher);
+    if (index > -1) {
+      this.publishers.splice(index, 1);
     }
-  
-    get_password(){
-      return "****";
-    }
-  
-    set_password(newPassword: string){
-      this.password = newPassword;
-    }
-  
-    showInfo(){
-      console.log(`Имя: ${this.name}`);
-      console.log(`Логин: ${this.login}`);
-    }
-  
-    eq(user:User): boolean{
-      return this.grade === user.grade;
-    }
-  
-    it(user:User): boolean{
-      return this.grade < user.grade;
-    }
-  
-    gt(user:User): boolean{
-      return this.grade > user.grade;
-    }
-  
-    }
-   class SuperUser {
-      public role: string;
-      public static count = 0;
-      name: string;
-      login: string;
-      grate:number;
-      password: string;
-      constructor(name: string, login: string, password: string, role:string, grate:number  ){
-          this.name = name
-          this.login = login
-          this.password = password
-          this.role = role
-          this.grate = grate
-          SuperUser.count++;
-  
-          }
-  
-           showInfo(){
-      console.log(`Имя: ${this.name}`);
-      console.log(`Логин: ${this.login}`);
-    }
-  
-      get_password(){
-      return "****";
-    }
-  
-      set_password(newPassword: string){
-      this.password = newPassword;
-    }
-  
-    getRole(): string {
-      return this.role
-    }
-    setRole(value: string) {
-      this.role = value;
-    }
-  
-   }
-  
-  //  const user1 = new User('Paul McCartney', 'paul', '1234', 3)
-  // const user2 = new User('George Harrison', 'george', '5678', 2)
-  // const user3 = new User('Richard Starkey', 'ringo', '8523', 3)
-  // const admin = new SuperUser('John Lennon', 'john', '0000', 'admin', 5)
-  
-  // user1.showInfo()
-  // admin.showInfo()
-  
-  // let users = User.count
-  // let admins = SuperUser.count
-  
-  // console.log(`Всего обычных пользователей: ${users}`)
-  // console.log(`Всего супер-пользователей: ${admins}`)
-  
+  }
+}
